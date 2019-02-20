71ea0d260136a26d5bf90ae33d46925701944f4e
Aditya Patwa <adityapatwa@live.com>
2019-02-20 14:06:05 -0500

    Added the feature of adding, editing and deleting recipes

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -3,7 +3,7 @@ import { Recipe } from '../recipe.model';
 import { ShoppingListService } from '../../shopping-list/shopping-list.service';
 import { Ingredient } from '../../shared/ingredient.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -16,7 +16,12 @@ export class RecipeDetailComponent implements OnInit {
   paramSubscription: Subscription;
   id: number;
 
-  constructor(private shoppingListService: ShoppingListService, private recipeService: RecipeService, private route: ActivatedRoute) {
+  constructor(
+    private shoppingListService: ShoppingListService,
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
   }
 
   ngOnInit() {
@@ -24,6 +29,11 @@ export class RecipeDetailComponent implements OnInit {
       this.id = +params['id'];
       this.recipe = this.recipeService.getRecipe(this.id);
     });
+  }
+
+  onDelete() {
+    this.recipeService.delteRecipe(this.id);
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 
   toShoppingList() {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,16 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
   private recipes: Recipe[] = [
-    new Recipe(1, 'A Test Recipe', 'This is a test recipe',
+    new Recipe('A Test Recipe', 'This is a test recipe',
       'https://upload.wikimedia.org/wikipedia/commons/8/89/7126_-_Luzern_-_Chicken_%26_pasta.JPG',
       [new Ingredient('Tomato', 5), new Ingredient('Chillies', 8)]),
-    new Recipe(2, 'Another Test Recipe', 'This is another test recipe',
+    new Recipe('Another Test Recipe', 'This is another test recipe',
       'https://upload.wikimedia.org/wikipedia/commons/8/89/7126_-_Luzern_-_Chicken_%26_pasta.JPG',
       [new Ingredient('Bread', 2), new Ingredient('Cucumber', 4)])
   ];
@@ -17,7 +19,22 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
-  getRecipe(id: number) {
-    return {...this.recipes.find((recipe => recipe.id === id))};
+  getRecipe(index: number) {
+    return {...this.recipes[index]};
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  delteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
