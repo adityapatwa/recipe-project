7b24499643f3ce1246aaaf8bedaca2ccfe1f188f
Aditya Patwa <adityapatwa@live.com>
2019-02-28 16:31:45 -0500

    Made further optimizations to the application by adding a core module that will hold all the components and sservices making the app module leaner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipeService } from './recipes/recipe.service';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { DataStorageService } from './shared/data-storage.service';
 import { HttpModule } from '@angular/http';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     AuthModule,
+    CoreModule,
     SharedModule,
     AppRoutingModule
   ],
-  providers: [AuthService, RecipeService, ShoppingListService, DataStorageService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {
