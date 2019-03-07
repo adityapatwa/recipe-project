f82d8a3624c5e12bd41c7b309a1ec419a6947a67
Aditya Patwa <adityapatwa@live.com>
2019-03-07 13:03:36 -0500

    Added ngrx to manage the state of the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
+import { StoreModule } from '@ngrx/store';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducers';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,7 @@ import { CoreModule } from './core/core.module';
     AuthModule,
     CoreModule,
     SharedModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
     AppRoutingModule
   ],
   bootstrap: [AppComponent]
