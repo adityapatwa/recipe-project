389597b871b1cd33993495553632fcd3fc4109fb
Aditya Patwa <adityapatwa@live.com>
2019-03-12 12:23:43 -0400

    Created a central AppState interface that will be used to manage the global application state

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -6,7 +6,7 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 import * as ShoppingListActions from '../../shopping-list/store/shopping-list.actions';
-import * as fromShoppingList from '../..//shopping-list/store/shopping-list.reducers';
+import * as fromApp from '../../store/app.reducers';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -22,7 +22,7 @@ export class RecipeDetailComponent implements OnInit {
     private recipeService: RecipeService,
     private route: ActivatedRoute,
     private router: Router,
-    private shoppingListStore: Store<fromShoppingList.AppState>
+    private shoppingListStore: Store<fromApp.AppState>
   ) {
   }
 
